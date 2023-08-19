5bec5ee09e848833c9cb4a12f3ba9a71c1f12ae7
Arvin <arvinrasheda121@gmail.com>
2023-08-19 23:14:36 +0700

    update: seeder

--- database/seeds/seeder.sql
+++ database/seeds/seeder.sql
@@ -1,32 +1,32 @@
-INSERT INTO "public"."kriteria" ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (2, 'C02', 'Kompetensi Kepribadian', 'BENEFIT', '15', '2023-08-19 01:52:56', '2023-08-19 01:52:56');
-INSERT INTO "public"."kriteria" ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (1, 'C01', 'Kompetensi Pedagogik', 'BENEFIT', '30', '2023-08-17 04:21:38', '2023-08-17 04:21:38');
-INSERT INTO "public"."kriteria" ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (3, 'C03', 'Kompetensi Sosial', 'BENEFIT', '10', '2023-08-19 01:53:31', '2023-08-19 01:53:31');
-INSERT INTO "public"."kriteria" ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (4, 'C04', 'Kompetensi Profesional', 'BENEFIT', '25', '2023-08-19 01:53:46', '2023-08-19 01:53:46');
-INSERT INTO "public"."kriteria" ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (5, 'C05', 'Kompetensi Kedisiplinan', 'BENEFIT', '20', '2023-08-19 01:54:04', '2023-08-19 01:54:04');
+INSERT INTO kriteria ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (2, 'C02', 'Kompetensi Kepribadian', 'BENEFIT', '15', '2023-08-19 01:52:56', '2023-08-19 01:52:56');
+INSERT INTO kriteria ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (1, 'C01', 'Kompetensi Pedagogik', 'BENEFIT', '30', '2023-08-17 04:21:38', '2023-08-17 04:21:38');
+INSERT INTO kriteria ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (3, 'C03', 'Kompetensi Sosial', 'BENEFIT', '10', '2023-08-19 01:53:31', '2023-08-19 01:53:31');
+INSERT INTO kriteria ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (4, 'C04', 'Kompetensi Profesional', 'BENEFIT', '25', '2023-08-19 01:53:46', '2023-08-19 01:53:46');
+INSERT INTO kriteria ("id", "kode_kriteria", "nama_kriteria", "attribute", "bobot", "created_at", "updated_at") VALUES (5, 'C05', 'Kompetensi Kedisiplinan', 'BENEFIT', '20', '2023-08-19 01:54:04', '2023-08-19 01:54:04');
 
 
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C01', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C01', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C01', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C01', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C01', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C01', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C01', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C01', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
 
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C02', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C02', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C02', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C02', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C02', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C02', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C02', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C02', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
 
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C03', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C03', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C03', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C03', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C03', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C03', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C03', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C03', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
 
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C04', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C04', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C04', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C04', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C04', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C04', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C04', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C04', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
 
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C05', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C05', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C05', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
-INSERT INTO "public"."nilai_kriteria" ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C05', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C05', 'Sangat Baik', '100', '2023-08-19 02:14:06', '2023-08-19 02:14:06');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C05', 'Cukup Baik', '50', '2023-08-19 02:24:44', '2023-08-19 02:24:44');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C05', 'Baik', '75', '2023-08-19 02:24:33', '2023-08-19 02:24:33');
+INSERT INTO nilai_kriteria ("kode_kriteria", "keterangan", "nilai", "created_at", "updated_at") VALUES ('C05', 'Kurang Baik', '25', '2023-08-19 02:25:10', '2023-08-19 02:25:10');
 
